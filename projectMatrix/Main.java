0d0587b2f369d38dca7b5aea40963c642650b210
Burak Unlu <burakaliunlu@gmail.com>
2023-02-16 19:09:07 -0500

    added code to run MatrixRunner to test project

--- projectMatrix/Main.java
+++ projectMatrix/Main.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        MatrixRunner matrixRunner = new MatrixRunner();
+        matrixRunner.main();
     }
 }
