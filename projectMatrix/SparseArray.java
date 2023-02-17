151c4753eecc350e51643280ffce188b48fa42e5
Burak Unlu <burakaliunlu@gmail.com>
2023-02-16 19:59:21 -0500

    added SparseArray.java

---
+++ projectMatrix/SparseArray.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SparseArray {
+
+    private int numRows;
+    private int numCols;
+    private double[][] values;
+    private Map<Integer, Map<Integer, Double>> sparseArray = new HashMap<>();
+
+
+    public SparseArray(int numRows, int numCols) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.values = new double[numRows][numCols];
+    }
+
+    public void set(int row, int col, double value) throws SparseArrayException{
+        if (row < 0 || row >= numRows || col < 0 || col >= numCols) {
+            throw new SparseArrayException("Index out of range.");
+        }
+        if (value != 0) {
+            if (!sparseArray.containsKey(row)) {
+                sparseArray.put(row, new HashMap<>());
+            }
+            sparseArray.get(row).put(col, value);
+        }
+    }
+
+    public double get(int row, int col) throws SparseArrayException{
+        if (row < 0 || row >= numRows || col < 0 || col >= numCols) {
+            throw new SparseArrayException("Index out of range.");
+        }
+        if (sparseArray.containsKey(row) && sparseArray.get(row).containsKey(col)) {
+            return sparseArray.get(row).get(col);
+        } else {
+            return 0;
+        }
+    }
+
+}
---
+++ projectMatrix/SparseArrayException.java
@@ -0,0 +1,14 @@
+/**
+* MatrixException
+* This exception is thrown for any error associated with the
+* methods in the Matrix class.
+* @author Burak Unlu
+* @version 2/16/2023
+*/
+public class SparseArrayException extends Exception{
+
+   public SparseArrayException() { super(); }
+
+   public SparseArrayException (String s) { super (s); }
+
+}
